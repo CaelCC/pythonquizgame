6e4fddce3d0ce3313a1d43495792f7f7ad996227
Cael Clarkson <caelclarkson@gmail.com>
2023-05-05 10:09:53 -0500

    update

--- quiz_game.py
+++ quiz_game.py
@@ -8,28 +8,28 @@ if playing.lower() != "yes":
 print("Okay! Let's play :P")
 score = 0
 
-answer = input("What come after the number 1? ")
+answer = input("What comes after 1? ")
 if answer == "2": 
     print('Awesome Sauce!')
     score += 1
 else:
     print("Wow! Are You OK? ;P")
 
-answer = input("What come after the letter a? ")
+answer = input("What comes after a? ")
 if answer.lower() == "b": 
     print('Awesome Sauce!')
     score += 1
 else:
     print("Wow! Are You OK? ;P")
 
-answer = input("What come after the before 2? ")
+answer = input("What comes before 2? ")
 if answer == "1": 
     print('Awesome Sauce!')
     score += 1
 else:
     print("Wow! Are You OK? ;P")
 
-answer = input("What come after the before b? ")
+answer = input("What comes before b? ")
 if answer.lower() == "a": 
     print('Awesome Sauce!')
     score += 1
